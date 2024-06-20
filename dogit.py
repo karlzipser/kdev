872457a5f2c4ddaf3fabe70f3e50591a960a415b
Karl Zipser <karlzipser@berkeley.edu>
2024-06-19 18:46:31 -0700

    gacp

---
+++ dogit.py
@@ -0,0 +1,28 @@
+## 79 ########################################################################
+from utilz2 import *
+args=dict(
+    task='',
+    repos=[
+        'projutils',
+        'kdev',
+        'tac',
+        'tac_attn',
+        'tac_ideal',
+        'kmodule',
+        'utilz2.2.102',
+    ],
+)
+gacp="git add .;git commit -m 'gacp';git push origin master"
+p=getparser(**args)
+for f in p.repos:
+    if p.task=='pull':
+        os_system('cd; cd',f,'; pwd; git pull',e=1,a=1)
+    elif p.task=='gacp':
+        os_system('cd; cd',f,'; pwd;',gacp,e=1,a=1)
+    else:
+        assert False
+
+
+
+## 79 ########################################################################
+#EOF

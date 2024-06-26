70c477b7fa434f9efc2580abf6f73fa2bf1665f5
Karl Zipser <karlzipser@berkeley.edu>
2024-06-25 17:18:51 -0700

    stable3

--- dogit.py
+++ dogit.py
@@ -12,6 +12,7 @@ args=dict(
         'kllm',
         'utilz2.2.102',
     ],
+    n=3,
 )
 p=getparser(**args)
 gacp="git add .;git commit -m 'gacp';git push origin "
@@ -20,6 +21,17 @@ for f in p.repos:
         os_system('cd; cd',f,'; pwd; git pull',e=1,a=1)
     elif p.task=='gacp':
         os_system('cd; cd',f,'; pwd;',gacp,e=1,a=1)
+    elif p.task=='stable':
+        os_system("""
+         cd;cd F;
+         git checkout -b stableN;
+         git add .;
+         git commit -m 'stableN';
+         git push -u origin stableN;
+         git checkout master;
+         git branch;
+         cd;
+        """.replace('F',f).replace('N',str(p.n)),a=1,e=1)
     else:
         assert False
 

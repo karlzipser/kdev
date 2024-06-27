79fe16ef2f6bb5ef6ca02568eebd45c5116a5764
Karl Zipser <karlzipser@berkeley.edu>
2024-06-26 22:14:23 -0700

    gacp

--- play_resnet.py
+++ play_resnet.py
@@ -6,15 +6,13 @@ import numpy as np
 import json
 import requests
 
-# Load the pretrained ResNet model
-model = models.resnet18(pretrained=True)
-
-# Set the model to evaluation mode
+#model = models.resnet18(pretrained=True)
+#torchvision.models.vit_b_16(weights=torchvision.models.ViT_B_16_Weights)
+model = torchvision.models.vit_b_16(weights=torchvision.models.ViT_B_16_Weights)#models.models.vit_b_16(pretrained=True)
 model.eval()
 from sampleimages import imgs
-g=imgs.sunny
+g=imgs.mtum
 g=cv2.resize(g,(224,224))
-# Generate a random noise image
 a=na([g.transpose(2,0,1)])
 b=torch.from_numpy(a).float()
 random_noise_image = b#torch.rand(1, 3, 224, 224)
@@ -25,24 +23,28 @@ if False:
 	normalized_image = normalize(random_noise_image[0])
 normalized_image=random_noise_image[0]/255
 
-# Add a batch dimension
 input_image = normalized_image.unsqueeze(0)
 
-# Pass the random noise image through the model
 with torch.no_grad():
     output = model(input_image)
 
-# Download ImageNet class labels
 LABELS_URL = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+
 response = requests.get(LABELS_URL)
 labels = response.json()
+so(opjD('imagenetlabels'),labels)
 
-# Get the predicted class index
 _, predicted_idx = torch.max(output, 1)
 
-# Get the class label
 predicted_label = labels[predicted_idx.item()]
 
-# Print the predicted label
 print(f"Predicted label: {predicted_label}",predicted_idx)
-plot(output[0,:].detach().cpu().numpy())
+#plot(output[0,:].detach().cpu().numpy())
+
+probabilities = torch.nn.functional.softmax(output[0], dim=0)
+confidence, predicted_idx = torch.max(probabilities, 0)
+predicted_label = labels[predicted_idx.item()]
+
+print(predicted_label, confidence.item())
+clf()
+plot(probabilities.detach().cpu().numpy())
